a307909e0d4100b756682a00baf63c540b98712a
kDawg Jet$ki <kandarpa9@gmail.com>
2019-07-20 12:44:26 -0400

    email check

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$con = mysqli_connect("localhost", "root", "", "socialmediadatabase");
+
+if(mysqli_connect_errno()) {
+    echo "Failed to connect".mysqli_connect_error();
+}
+$query = mysqli_query($con, "INSERT INTO test VALUES('', 'Kandarp')");
+
+?>
+<html>
+<head>
+    <title>Kandarp</title>
+</head>
+<body>
+Hello Kandarp!!
+</body>
+</html>
--- register.php
+++ register.php
@@ -1,4 +1,66 @@
 <?php
+$con = mysqli_connect("localhost", "root", "", "socialmediadatabase");
+
+if(mysqli_connect_errno()) {
+    echo "Failed to connect".mysqli_connect_error();
+}
+
+// variables for register form
+$firstName = "";
+$lastName = "";
+$email = "";
+$confirmEmail = "";
+$password = "";
+$confirmPassword = "";
+$signupDate = "";
+$error_array = "";
+
+if(isset($_POST['register_button'])) {
+
+    // registeration form values
+    
+    // remove html tags
+    $firstName = strip_tags($_POST['reg_firstname']);
+
+    // remove spaces
+    $firstName = str_replace(' ', '', $firstName);
+
+    // uppercase first letter
+    $firstName = ucfirst(strtolower($firstName));
+
+    
+    $lastName = strip_tags($_POST['reg_lastname']);
+
+    $lastName = str_replace(' ', '', $lastName);
+
+    $lastName = ucfirst(strtolower($lastName));
+
+    $email = strip_tags($_POST['reg_email']); 
+
+    $email = str_replace(' ', '', $email);
+
+    $email = ucfirst(strtolower($email));
+
+    $confirmEmail = strip_tags($_POST['reg_confirmemail']); 
+
+    $confirmEmail = str_replace(' ', '', $confirmEmail);
+
+    $confirmEmail = ucfirst(strtolower($confirmEmail));
+
+    $password = strip_tags($_POST['reg_password']); 
+
+    $confirmpassword = strip_tags($_POST['reg_confirmpassword']); 
+    
+    // current date
+    $signupDate = date("Y-m-d");
+
+    if($email == $confirmEmail) {
+
+    }
+    else {
+        echo "Emails don't match";
+    }
+}
 
 ?>
 
@@ -8,17 +70,17 @@
 </head>
 <body>
     <form action="register.php" method="POST">
-        <input type="text" name="reg_fname" placeholder="First Name" required>
+        <input type="text" name="reg_firstname" placeholder="First Name" required>
         <br>
-        <input type="text" name="reg_lname" placeholder="First Name" required>
+        <input type="text" name="reg_lastname" placeholder="First Name" required>
         <br>
         <input type="email" name="reg_email" placeholder="Email" required>
         <br>
-        <input type="email" name="reg_email2" placeholder="Confirm Email" required>
+        <input type="email" name="reg_confirmemail" placeholder="Confirm Email" required>
         <br>
         <input type="password" name="reg_password" placeholder="Password" required>
         <br>
-        <input type="password" name="reg_password2" placeholder="Confirm Password" required>
+        <input type="password" name="reg_confirmpassword" placeholder="Confirm Password" required>
         <br>
         <input type="submit" name="register_button" value="Register">
     </form>
