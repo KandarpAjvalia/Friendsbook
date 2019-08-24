e2081ffb770c459892c463335ff90bef2eec1e47
kDawg Jet$ki <kandarpa9@gmail.com>
2019-08-23 23:58:00 -0400

    login signup slide up and down

--- assets/js/register.js
+++ assets/js/register.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
-    // On click signup, hide login and show registeration form
+    //On click signup, hide login and show registeration form
     $("#signup").click(function() {
         $("#login_content").slideUp("slow", function() {
             $("#signup_content").slideDown("slow");
         });
     });
 
+    //On click signin, hide registerationi and show login form
     $("#signin").click(function() {
         $("#signup_content").slideUp("slow", function() {
             $("#login_content").slideDown("slow");
         });
     });
-
 });
